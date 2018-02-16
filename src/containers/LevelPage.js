ade171aff3fb5ec1faee50a910889040f5857d15
Stefan Aleksic <stefan@stefanaleksic.com>
2018-02-16 18:51:26 -0500

    Refactor create react app to not use external App.

---
+++ src/containers/LevelPage.js
@@ -0,0 +1,9 @@
+import React, {PureComponent} from 'react';
+
+export default class LevelPage extends PureComponent {
+  render() {
+    return (
+      <h1> yay</h1>
+    )
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import LevelPage from './containers/LevelPage';
+import GameOverPage from './containers/GameOverPage';
+import {
+    BrowserRouter as Router,
+    Route,
+} from 'react-router-dom';
+
+
+const App = () =>
+  <Router basename={process.env.PUBLIC_URL + '/'}>
+    <div>
+      <Route exact path="/" component={LevelPage} />
+      <Route exact path="/gameover" component={GameOverPage} />
+    </div>
+  </Router>
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
