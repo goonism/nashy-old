825a94a41a9d20d976edae7954934343279ed88b
Stefan Aleksic <stefan@stefanaleksic.com>
2018-02-16 19:14:29 -0500

    Update game over page.

--- src/containers/GameOverPage.js
+++ src/containers/GameOverPage.js
@@ -3,7 +3,10 @@ import React, {PureComponent} from 'react';
 export default class GameOverPage extends PureComponent {
   render() {
     return (
-      <h2> u suck </h2>
+      <section>
+        <p> hey, you lost. someone gave you the wrong number. thanks for playing! </p>
+        <p> made with {'<3'} by <a href="https://twitter.com/LGVichy">{'@'}lgvichy</a> and <a href="https://twitter.com/stayfun_">{'@'}stayfun_</a> in the frozen tundra of Rochester, New York</p>
+      </section>
     )
   }
 }
